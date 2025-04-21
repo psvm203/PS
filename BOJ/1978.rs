d948fa8f3782f4f794e26f5866c6e08a3dcfc5b8
psvm203 <psvm203@proton.me>
2025-04-21 20:25:22 +0900

    250421

---
+++ BOJ/1978.rs
@@ -0,0 +1,26 @@
+use std::io::*;
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+
+    let numbers = input
+        .split_ascii_whitespace()
+        .skip(1)
+        .flat_map(|x| x.parse::<usize>());
+
+    let mut is_prime = [true; 1001];
+    is_prime[0] = false;
+    is_prime[1] = false;
+
+    for i in 2..1001 {
+        if is_prime[i] {
+            for j in (i * i..1001).step_by(i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+
+    let primes_count = numbers.filter(|&number| is_prime[number]).count();
+
+    print!("{primes_count}");
+}
---
+++ BOJ/30802.rs
@@ -0,0 +1,30 @@
+use std::io::*;
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+    let mut lines = input.lines();
+
+    let n: usize = lines.next().unwrap().parse().unwrap();
+
+    let mut read_ints = || {
+        lines
+            .next()
+            .unwrap()
+            .split_ascii_whitespace()
+            .flat_map(|x| x.parse())
+            .collect::<Vec<usize>>()
+    };
+
+    let sizes = read_ints();
+
+    let (t, p) = {
+        let vec = read_ints();
+        (vec[0], vec[1])
+    };
+
+    let tshirt_bundle: usize = sizes.iter().map(|size| (size + t - 1) / t).sum();
+    let pen_bundle = n / p;
+    let pen = n % p;
+
+    print!("{tshirt_bundle}\n{pen_bundle} {pen}");
+}
