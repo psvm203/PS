c4ea2920452672d31510b312d365ba816afecbb9
psvm203 <psvm203@proton.me>
2025-05-19 22:48:33 +0900

    250519

--- BOJ/10989.rs
+++ BOJ/10989.rs
@@ -1,21 +1,20 @@
 use std::io::*;
 
 fn main() {
-    let n: usize = {
+    let read_int = || -> usize {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
         input.trim().parse().unwrap()
     };
 
+    let n = read_int();
+
     let mut counts = [0; 10001];
 
     for _ in 0..n {
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
-
-        let number: usize = input.trim().parse().unwrap();
-        counts[number] += 1;
+        let num = read_int();
+        counts[num] += 1;
     }
 
     let mut output = BufWriter::new(stdout());
