5bf3c06f50f0a288d0d4951d4f397b76f666f05f
psvm203 <psvm203@proton.me>
2025-07-17 07:06:21 +0900

    250717

---
+++ BOJ/5522.rs
@@ -0,0 +1,12 @@
+use std::io::*;
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+
+    let sum: usize = stdin
+        .split_ascii_whitespace()
+        .flat_map(|x| x.parse::<usize>())
+        .sum();
+
+    println!("{sum}");
+}
