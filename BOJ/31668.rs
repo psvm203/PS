22d6c9cdc7c9679343c3472a158464b2106b1e12
psvm203 <psvm203@proton.me>
2025-09-06 21:59:26 +0900

    250906

---
+++ BOJ/31668.rs
@@ -0,0 +1,17 @@
+use std::convert::*;
+use std::io::*;
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+
+    let [n, m, k] = stdin
+        .split_ascii_whitespace()
+        .flat_map(|x| x.parse())
+        .collect::<Vec<usize>>()
+        .try_into()
+        .unwrap();
+
+    let output = m / n * k;
+
+    println!("{output}");
+}
